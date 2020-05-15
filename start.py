8075cc5718aa02e300303a15c499eae3ba9b57e0
Julian Vos <post@julianvos.nl>
2020-05-15 14:18:01 +0200

    Action is now sent on button release, not press

--- start.py
+++ start.py
@@ -9,24 +9,28 @@ button1 = Button(18)
 button2 = Button(23)
 
 while True:
-  if button1.is_pressed:
-    if button1pressed != True:
-      print("Sending obs action")
-      requests.get(url = "http://192.168.2.27:3000/macro", params = { 'action': 'obs' })
-      button1pressed = True
-      print("OBS request sent, button1pressed is true")
-  else:
-    if button1pressed:
-      button1pressed = False
-      print("Button 1 released, is now False")
+    if button1.is_pressed:
+        if button1pressed != True:
+            button1pressed = True
+            print("Button 1 pressed, is now true")
+    else:
+        if button1pressed:
+            print("Sending obs action")
+            requests.get(url="http://192.168.2.27:3000/macro",
+                         params={'action': 'obs'})
+            print("OBS request sent")
+            button1pressed = False
+            print("Button 1 released, is now False")
 
-  if button2.is_pressed:
-    if button2pressed != True:
-      print("Sending discord action")
-      requests.get(url = "http://192.168.2.27:3000/macro", params = { 'action': 'discord' })
-      button2pressed = True
-      print("Discord action sent, button2pressed is True")
-  else:
-    if button2pressed:
-      button2pressed = False
-      print("Button 2 released, is now False")
+    if button2.is_pressed:
+        if button2pressed != True:
+            button2pressed = True
+            print("Button 2 pressed, is now True")
+    else:
+        if button2pressed:
+            print("Sending discord action")
+            requests.get(url="http://192.168.2.27:3000/macro",
+                         params={'action': 'discord'})
+            print("Discord request sent")
+            button2pressed = False
+            print("Button 2 released, is now False")
