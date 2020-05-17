20a9c140f996e91c57965fe5b3bc42a32466d953
Julian Vos <post@julianvos.nl>
2020-05-18 00:24:00 +0200

    Added 3 more buttons

--- start.py
+++ start.py
@@ -4,9 +4,16 @@ import requests
 
 button1pressed = False
 button2pressed = False
+button3pressed = False
+button4pressed = False
+button5pressed = False
 
 button1 = Button(18)
 button2 = Button(23)
+button3 = Button(12)
+button4 = Button(25)
+button5 = Button(16)
+
 
 while True:
     if button1.is_pressed:
@@ -34,3 +41,42 @@ while True:
             print("Discord request sent")
             button2pressed = False
             print("Button 2 released, is now False")
+
+    if button3.is_pressed:
+        if button3pressed != True:
+            button3pressed = True
+            print("Button 3 pressed, is now True")
+    else:
+        if button3pressed:
+            print("Sending discord action")
+            requests.get(url="http://192.168.2.27:3000/macro",
+                         params={'action': 'toggleplay'})
+            print("Discord request sent")
+            button3pressed = False
+            print("Button 3 released, is now False")
+    
+    if button4.is_pressed:
+        if button4pressed != True:
+            button4pressed = True
+            print("Button 4 pressed, is now True")
+    else:
+        if button4pressed:
+            print("Sending discord action")
+            requests.get(url="http://192.168.2.27:3000/macro",
+                         params={'action': 'previous'})
+            print("Discord request sent")
+            button4pressed = False
+            print("Button 4 released, is now False")
+
+    if button5.is_pressed:
+        if button5pressed != True:
+            button5pressed = True
+            print("Button 5 pressed, is now True")
+    else:
+        if button5pressed:
+            print("Sending discord action")
+            requests.get(url="http://192.168.2.27:3000/macro",
+                         params={'action': 'next'})
+            print("Discord request sent")
+            button5pressed = False
+            print("Button 5 released, is now False")
